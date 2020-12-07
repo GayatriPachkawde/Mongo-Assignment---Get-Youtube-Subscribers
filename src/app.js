1cdd2689bcaf042d1ee925b9737090cd7c3a7abf
GayatriPachkawde <gayatri5kawde@gmail.com>
2020-12-07 17:24:22 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,28 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const subscriberModel = require("./models/subscribers");
+const data = require("./data");
 
-const express = require('express');
-const app = express()
+const db = mongoose.connection;
 
+app.get("/subscribers", async (req, res) => {
+  const subscribers = await subscriberModel.find({});
+  res.send(subscribers);
+});
 
-// Your code goes here
+app.get("/subscribers/names", async (req, res) => {
+  const subscribers = await subscriberModel.find({});
 
+  const names = subscribers.map((subscriber) => {
+    return {
+      name: subscriber.name,
+      subscribedChannel: subscriber.subscribedChannel,
+    };
+  });
 
+  res.send(names);
+});
 
+app.get("/subscribers/:id", async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const subscriber = await subscriberModel.findById(id);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.send(subscriber);
+  } catch (e) {
+    res.sendStatus(404).send({ message: error.message });
+    // res.send({ message: error.message });
+  }
+});
 module.exports = app;
